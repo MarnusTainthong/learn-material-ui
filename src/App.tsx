c7d6d3be7f57ed3b9d81b477f16f2911e31c836a
MMFFMFM_ <marnusfrank@gmail.com>
2024-10-09 17:13:57 +0700

    lesson-06

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import { Button, CssBaseline, TextField } from "@mui/material";
+import { Box, Button, CssBaseline, Stack, TextField } from "@mui/material";
 
 function App() {
   return (
     <>
       <CssBaseline />
-      <div>
-        <TextField
-          id="emailInput"
-          name="emailInput"
-          label="Email"
-          type="email"
-          placeholder="please enter email"
-          autoComplete="email"
-          autoFocus
-          required
-          fullWidth
-          variant="outlined"
-        />
-      </div>
-      <div>
-        <Button variant="contained">Hello world</Button>
-      </div>
+      <Box
+        sx={{
+          padding: "2rem",
+          maxWidth: "400px",
+          margin: "auto",
+          marginTop: "10vh",
+          border: "1px solid #eee",
+          boxShadow: "0 2px 10px rgba(0, 0, 0, 0.08)",
+          borderRadius: "4px",
+        }}
+      >
+        <Stack spacing={2}>
+          <div>
+            <TextField
+              id="emailInput"
+              name="emailInput"
+              label="Email"
+              type="email"
+              placeholder="please enter email"
+              autoComplete="email"
+              autoFocus
+              required
+              fullWidth
+              variant="outlined"
+            />
+          </div>
+          <div>
+            <TextField
+              id="password"
+              name="password"
+              label="Password"
+              type="password"
+              placeholder="Please enter password"
+              required
+              fullWidth
+              variant="outlined"
+            />
+          </div>
+          <div>
+            <Button variant="contained">Hello world</Button>
+          </div>
+        </Stack>
+      </Box>
     </>
   );
 }
